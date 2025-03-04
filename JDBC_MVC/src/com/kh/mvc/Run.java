09b310d4719e01fc276000cab811442bd1e79399
kkm <rhksalsdldia@naver.com>
2025-03-05 00:00:17 +0900

    test2

---
+++ JDBC_MVC/src/com/kh/mvc/Run.java
@@ -0,0 +1,16 @@
+package com.kh.mvc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		// 프로그램이 시작되는 시작점
+	}
+
+}
